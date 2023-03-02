be0050157ac9d805bdae7102de0bcd0fe9087ab7
usaginotsuki <jcchachalo@gmail.com>
2023-03-02 00:20:01 -0500

    Multi form step created for found pet

--- lib/pages/lost_found_pets/found_form.page.dart
+++ lib/pages/lost_found_pets/found_form.page.dart
@@ -29,11 +29,12 @@ class _FoundFormState extends State<FoundForm> {
   bool dateSelected = false;
   DateTime date = DateTime.now();
   late File _image;
+  bool imageSelected = false;
+  int currentStep = 0;
 
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context);
-
     return Scaffold(
       key: _scaffoldKey,
       drawer: drawerMenu(context),
@@ -41,145 +42,203 @@ class _FoundFormState extends State<FoundForm> {
         title: const Text("Formulario de mascotas encontradas"),
         toolbarOpacity: 0.8,
       ),
-      body: SingleChildScrollView(
-        child: Form(
-          key: formKey,
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Column(
-                children: [
-                  SelectFormField(
-                    style: const TextStyle(color: Colors.black, fontSize: 20),
-                    type: SelectFormFieldType.dropdown, // or can be dialog
-                    labelText: 'Que tipo de mascota es?',
-                    items: _categories,
-                    onChanged: (val) => print(val),
-                    onSaved: (val) => print(val),
-                  ),
-                  const Padding(padding: EdgeInsets.only(top: 50)),
-                  GestureDetector(
-                    child: dateSelected
-                        ? Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 1,
-                              ),
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            padding: const EdgeInsets.all(10),
-                            child: Column(
-                              children: [
-                                Text(
-                                  'Fecha y hora de la publicacion: ',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                  textAlign: TextAlign.left,
-                                ),
-                                Row(
-                                  children: [
-                                    Icon(Icons.calendar_today),
-                                    SizedBox(width: 10),
-                                    Text(
-                                        '${date.day}/${date.month}/${date.year} \t : \t ${date.hour}:${date.minute}'),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          )
-                        : Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 1,
-                              ),
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            padding: const EdgeInsets.all(10),
-                            child: Row(
-                              children: [
-                                Icon(Icons.calendar_today),
-                                SizedBox(width: 10),
-                                Text('Cuando lo encontraste?'),
-                              ],
-                            ),
-                          ),
-                    onTap: () {
-                      DatePicker.showDateTimePicker(context,
-                          showTitleActions: true,
-                          minTime:
-                              DateTime.now().subtract(const Duration(days: 3)),
-                          maxTime:
-                              DateTime.now().subtract(const Duration(days: 1)),
-                          onChanged: (date) {}, onConfirm: (date) {
-                        if (date.millisecondsSinceEpoch >
-                            DateTime.now().millisecondsSinceEpoch + 10) {
-                          setState(() {
-                            dateSelected = false;
-                            date = DateTime.now();
-                          });
+      body: Stepper(
+        steps: getSteps(),
+        type: StepperType.horizontal,
+        currentStep: currentStep,
+        onStepTapped: (step) {
+          setState(() {
+            currentStep = step;
+          });
+        },
+        onStepContinue: () {
+          setState(() {
+            if (currentStep < getSteps().length - 1) {
+              currentStep = currentStep + 1;
+            } else {
+              currentStep = 0;
+            }
+          });
+        },
+        onStepCancel: () {
+          setState(() {
+            if (currentStep > 0) {
+              currentStep = currentStep - 1;
+            } else {
+              currentStep = 0;
+            }
+          });
+        },
+      ),
+    );
+  }
 
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text(
-                                  'La fecha seleccionada no puede ser mayor a la fecha actual'),
-                            ),
-                          );
-                        } else {
-                          dateSelected = true;
-                          setState(() {
-                            this.date = date;
-                            dev.log(date.toString());
-                          });
-                        }
-                      }, currentTime: DateTime.now(), locale: LocaleType.es);
-                    },
-                  ),
-                  const Padding(padding: EdgeInsets.only(top: 50)),
-                  GestureDetector(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 1,
-                        ),
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      child: Row(
-                        children: [
-                          Icon(Icons.camera_alt),
-                          SizedBox(width: 10),
-                          Text('Sube una foto de la mascota'),
-                        ],
-                      ),
+  List<Step> getSteps() {
+    final screenSize = MediaQuery.of(context);
+
+    return [
+      Step(
+        state: currentStep >= 0 ? StepState.complete : StepState.disabled,
+        isActive: currentStep >= 0,
+        title: Text("Paso 1"),
+        content: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Center(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                child: Column(
+                  children: [
+                    SelectFormField(
+                      style: const TextStyle(color: Colors.black, fontSize: 20),
+                      type: SelectFormFieldType.dropdown, // or can be dialog
+                      labelText: 'Que tipo de mascota es?',
+                      items: _categories,
+                      onChanged: (val) => print(val),
+                      onSaved: (val) => print(val),
                     ),
-                    onTap: () async {
-                      final pickedFile = await _picker.pickImage(
-                          source: ImageSource.gallery,
-                          imageQuality: 50,
-                          maxWidth: 600);
+                    const Padding(padding: EdgeInsets.only(top: 50)),
+                    GestureDetector(
+                      child: dateSelected
+                          ? Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 1,
+                                ),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Fecha y hora de la publicacion: ',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Icon(Icons.calendar_today),
+                                      SizedBox(width: 10),
+                                      Text(
+                                          '${date.day}/${date.month}/${date.year} \t : \t ${date.hour}:${date.minute}'),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            )
+                          : Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 1,
+                                ),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                children: [
+                                  Icon(Icons.calendar_today),
+                                  SizedBox(width: 10),
+                                  Text('Cuando lo encontraste?'),
+                                ],
+                              ),
+                            ),
+                      onTap: () {
+                        DatePicker.showDateTimePicker(context,
+                            showTitleActions: true,
+                            minTime: DateTime.now()
+                                .subtract(const Duration(days: 3)),
+                            maxTime: DateTime.now()
+                                .subtract(const Duration(days: 1)),
+                            onChanged: (date) {}, onConfirm: (date) {
+                          if (date.millisecondsSinceEpoch >
+                              DateTime.now().millisecondsSinceEpoch + 10) {
+                            setState(() {
+                              dateSelected = false;
+                              date = DateTime.now();
+                            });
 
-                      if (pickedFile != null) {
-                        setState(() {
-                          _image = File(pickedFile.path);
-                          dev.log(_image.path);
-                          
-                        });
-                      } else {
-                        dev.log('No image selected.');
-                      }
-                    },
-                  ),
-                  Container(
-                    child: const Placeholder(),
-                  )
-                ],
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text(
+                                    'La fecha seleccionada no puede ser mayor a la fecha actual'),
+                              ),
+                            );
+                          } else {
+                            dateSelected = true;
+                            setState(() {
+                              this.date = date;
+                              dev.log(date.toString());
+                            });
+                          }
+                        }, currentTime: DateTime.now(), locale: LocaleType.es);
+                      },
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 50)),
+                    GestureDetector(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          children: [
+                            Icon(Icons.camera_alt),
+                            SizedBox(width: 10),
+                            Text(imageSelected
+                                ? 'Cambiar imagen'
+                                : 'Agregar imagen'),
+                          ],
+                        ),
+                      ),
+                      onTap: () async {
+                        final pickedFile = await _picker.pickImage(
+                            source: ImageSource.gallery,
+                            imageQuality: 50,
+                            maxWidth: 600);
+
+                        if (pickedFile != null) {
+                          setState(() {
+                            _image = File(pickedFile.path);
+                            dev.log(_image.path);
+                            imageSelected = true;
+                          });
+                        } else {
+                          dev.log('No image selected.');
+                        }
+                      },
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 30)),
+                    Container(
+                      child: imageSelected
+                          ? Image.file(
+                              _image,
+                              width: screenSize.size.width * 0.8,
+                              height: screenSize.size.height * 0.4,
+                              fit: BoxFit.cover,
+                            )
+                          : Text(''),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
-    );
+      Step(
+        state: currentStep >= 1 ? StepState.complete : StepState.disabled,
+        title: Text("Paso 2"),
+        content: Text("Paso 2"),
+        isActive: currentStep >= 1,
+      ),
+    ];
   }
 }
